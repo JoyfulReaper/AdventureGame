7b2f1acad34d212cc910cd6241bd4c07f2bd72fc
Kyle Givler <xvegan88x@gmail.com>
2023-08-22 18:19:04 -0400

    Initial commit

---
+++ AdventureGame/AdventureGame/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace AdventureGame;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        outputTB = new TextBox();
+        EBtn = new Button();
+        NBtn = new Button();
+        WBtn = new Button();
+        SBtn = new Button();
+        LookBtn = new Button();
+        SuspendLayout();
+        // 
+        // outputTB
+        // 
+        outputTB.Location = new Point(12, 12);
+        outputTB.Multiline = true;
+        outputTB.Name = "outputTB";
+        outputTB.Size = new Size(557, 257);
+        outputTB.TabIndex = 0;
+        // 
+        // EBtn
+        // 
+        EBtn.Location = new Point(494, 309);
+        EBtn.Name = "EBtn";
+        EBtn.Size = new Size(75, 23);
+        EBtn.TabIndex = 1;
+        EBtn.Text = "E";
+        EBtn.UseVisualStyleBackColor = true;
+        EBtn.Click += EBtn_Click;
+        // 
+        // NBtn
+        // 
+        NBtn.Location = new Point(454, 280);
+        NBtn.Name = "NBtn";
+        NBtn.Size = new Size(75, 23);
+        NBtn.TabIndex = 2;
+        NBtn.Text = "N";
+        NBtn.UseVisualStyleBackColor = true;
+        NBtn.Click += NBtn_Click;
+        // 
+        // WBtn
+        // 
+        WBtn.Location = new Point(400, 309);
+        WBtn.Name = "WBtn";
+        WBtn.Size = new Size(75, 23);
+        WBtn.TabIndex = 3;
+        WBtn.Text = "W";
+        WBtn.UseVisualStyleBackColor = true;
+        WBtn.Click += WBtn_Click;
+        // 
+        // SBtn
+        // 
+        SBtn.Location = new Point(454, 338);
+        SBtn.Name = "SBtn";
+        SBtn.Size = new Size(75, 23);
+        SBtn.TabIndex = 4;
+        SBtn.Text = "S";
+        SBtn.UseVisualStyleBackColor = true;
+        SBtn.Click += SBtn_Click;
+        // 
+        // LookBtn
+        // 
+        LookBtn.BackColor = SystemColors.ControlLight;
+        LookBtn.Location = new Point(12, 275);
+        LookBtn.Name = "LookBtn";
+        LookBtn.Size = new Size(75, 23);
+        LookBtn.TabIndex = 5;
+        LookBtn.Text = "Look";
+        LookBtn.UseVisualStyleBackColor = true;
+        LookBtn.Click += LookBtn_Click;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(7F, 17F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(581, 382);
+        Controls.Add(LookBtn);
+        Controls.Add(SBtn);
+        Controls.Add(WBtn);
+        Controls.Add(NBtn);
+        Controls.Add(EBtn);
+        Controls.Add(outputTB);
+        Name = "Form1";
+        Text = "Form1";
+        ResumeLayout(false);
+        PerformLayout();
+    }
+
+    #endregion
+
+    private TextBox outputTB;
+    private Button EBtn;
+    private Button NBtn;
+    private Button WBtn;
+    private Button SBtn;
+    private Button LookBtn;
+}
---
+++ AdventureGame/AdventureGame/Form1.cs
@@ -0,0 +1,121 @@
+namespace AdventureGame;
+
+public partial class Form1 : Form
+{
+
+    /*
+    * 
+    * Troll Room -- Forest
+    *    |
+    *  Cave  -----  Dungeon  
+    * */
+
+    Room room0;
+    Room room1;
+    Room room2;
+    Room room3;
+
+    Room[] map;
+
+    int position;
+
+    public Form1()
+    {
+        InitializeComponent();
+        InitGame();
+        StartGame();
+    }
+
+    private void InitGame()
+    {
+        room0 = new Room();
+        room1 = new Room();
+        room2 = new Room();
+        room3 = new Room();
+
+        map = new Room[4];
+
+        map[0] = room0;
+        map[1] = room1;
+        map[2] = room2;
+        map[3] = room3;
+
+        room0.Name = "Troll Room";
+        room0.Description = "a dank, dark room that smells of troll";
+        room0.n = -1;
+        room0.s = 2;
+        room0.w = -1;
+        room0.e = 1;
+
+        room1.Name = "Forest";
+        room1.Description = "a light, airy forest shimmering with sunlight";
+        room1.n = -1;
+        room1.s = -1;
+        room1.w = 0;
+        room1.e = -1;
+
+        room2.Name = "Cave";
+        room2.Description = "a vast cave with walls covered by luminous moss";
+        room2.n = 0;
+        room2.s = -1;
+        room2.w = -1;
+        room2.e = 3;
+
+        room3.Name = "Dungeon";
+        room3.Description = "a gloomy dungeon. Rats scurry across its floor";
+        room3.n = -1;
+        room3.s = -1;
+        room3.w = 2;
+        room3.e = -1;
+
+        position = 0;
+    }
+
+    private void StartGame()
+    {
+        outputTB.Text = $"Welcome to the Great Adventure!\r\n";
+        outputTB.AppendText($"You are in the {map[position].Name}.");
+        outputTB.AppendText($"It is {map[position].Description}\r\n");
+        outputTB.AppendText("Where do you want to go now?\r\n");
+        outputTB.AppendText("Click a direction button: N, S, W or E.\r\n");
+    }
+
+    private void MovePlayer(int newpos)
+    {
+        if (newpos == -1)
+        {
+            outputTB.Text = "There is no exit in that direction\r\n";
+        }
+        else
+        {
+            position = newpos;
+            outputTB.Text = $"You are now in the {map[position].Name}\r\n";
+        }
+    }
+
+    private void NBtn_Click(object sender, EventArgs e)
+    {
+        MovePlayer(map[position].n);
+    }
+
+    private void SBtn_Click(object sender, EventArgs e)
+    {
+        MovePlayer(map[position].s);
+    }
+
+    private void WBtn_Click(object sender, EventArgs e)
+    {
+        MovePlayer(map[position].w);
+    }
+
+    private void EBtn_Click(object sender, EventArgs e)
+    {
+        MovePlayer(map[position].e);
+    }
+
+    private void LookBtn_Click(object sender, EventArgs e)
+    {
+        outputTB.Text = $"You are in the {map[position].Name}.\r\n";
+        outputTB.AppendText($"It is {map[position].Description}\r\n");
+    }
+}
